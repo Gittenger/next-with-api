0f9758ad15258d531be219aea76fa44dc7ba3915
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-06-20 20:17:12 -0400

    get user/ dbConnect  success

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,16 +1,33 @@
-export default (req, res) => {
+import User from '../../../models/userSchema'
+import dbConnect from '../../../utils/dbConnect'
+
+export default async function handler(req, res) {
 	const {
 		query: { id },
+		method,
 	} = req
 
-	const user = (() => (id === '1' || id === '2' ? id : null))()
+	await dbConnect()
 
-	if (!user) {
-		return res.status(404).json({
-			status: 404,
-			message: 'Not Found',
-		})
+	switch (method) {
+		case 'GET':
+			try {
+				const user = await User.findById(id)
+
+				if (!user) {
+					return res.status(404).json({
+						status: 404,
+						message: 'Not Found',
+					})
+				}
+				res.status(200).json({ user })
+			} catch (err) {
+				res.status(400).json({ success: false, err })
+			}
+			break
+
+		default:
+			res.status(400).json({ success: false })
+			break
 	}
-
-	res.status(200).json({ user: { id, name: 'Test user' } })
 }
