d75da86fac29a5ecb293e715a9efb8fe81ddde84
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-06-21 13:22:57 -0400

    signup api

---
+++ pages/api/users/signup.js
@@ -0,0 +1,41 @@
+import dbConnect from '../../../utils/dbConnect'
+import authToken from '../../../utils/authToken'
+import User from '../../../models/userSchema'
+
+const { createAndSendToken, signToken } = authToken
+
+export default async function handler(req, res) {
+	const {
+		query,
+		method,
+		body: { name, email, password, passwordConfirm },
+	} = req
+
+	await dbConnect()
+
+	switch (method) {
+		case 'POST':
+			try {
+				const user = await User.create({
+					name,
+					email,
+					password,
+					passwordConfirm,
+				})
+
+				createAndSendToken(user, 200, req, res)
+			} catch (err) {
+				res.status(400).json({
+					success: false,
+					err,
+				})
+			}
+			break
+
+		default:
+			res.status(400).json({
+				success: false,
+			})
+			break
+	}
+}
---
+++ utils/authToken.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken'
+
+export const signToken = id =>
+	jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN | 0,
+	})
+
+export const createAndSendToken = (user, statusCode, req, res) => {
+	const token = signToken(user._id)
+	// const cookieOptions = {
+	// 	expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 1000),
+	// 	httpOnly: true,
+	// }
+
+	// if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
+	// res.cookie('jwt', token, cookieOptions)
+
+	user.password = undefined
+
+	const { id, role, name, email } = user
+
+	res.status(statusCode).json({
+		success: true,
+		token,
+		user: {
+			id,
+			role,
+			name,
+			email,
+		},
+	})
+}
+
+const authToken = {
+	signToken,
+	createAndSendToken,
+}
+
+export default authToken
