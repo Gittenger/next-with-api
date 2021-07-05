d9f1d273ad6bb327b93ab255e70aa5596a544a85
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-05 13:58:08 -0400

    restrict functionality

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,8 @@
 import User from '../../models/userSchema'
 import dbConnect from '../../utils/dbConnect'
+import authToken from '../../utils/authToken'
+
+const { protect } = authToken
 
 export default async function handler(req, res) {
 	const { method } = req
@@ -9,6 +12,14 @@ export default async function handler(req, res) {
 	switch (method) {
 		case 'GET':
 			try {
+				const authUser = await protect(req, res)
+
+				if (authUser.role !== 'admin') {
+					return res.status(403).json({
+						success: false,
+						message: 'You do not have permission to perform this action',
+					})
+				}
 				const users = await User.find()
 
 				res.status(200).json({
