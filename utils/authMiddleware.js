e346a25622a5e4974a1c55df078ba56cc214b85d
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-08 15:40:43 -0400

    protect mw func

---
+++ utils/authMiddleware.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+import User from '../models/userSchema'
+
+export const protect = async (req, res, next) => {
+	let token
+
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		token = req.headers.authorization.split(' ')[1]
+	} else if (req.cookies.jwt) {
+		token = req.cookies.jwt
+	}
+
+	if (!token) {
+		return res.status(401).json({
+			success: false,
+			message: 'You are not logged in. Please log in for access',
+		})
+	}
+
+	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+	const currentUser = await User.findById(decoded.id)
+	if (!currentUser) {
+		return res.status(401).json({
+			success: false,
+			message: 'The user belonging to this token no longer exists',
+		})
+	}
+
+	if (currentUser.changedPasswordAfter(decoded.iat)) {
+		return res.status(401).json({
+			success: false,
+			message: 'User recently changed password. Please log in again.',
+		})
+	}
+
+	req.user = currentUser
+	next()
+}
