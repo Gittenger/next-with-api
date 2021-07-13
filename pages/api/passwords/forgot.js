30c9de8bacac55eada44ff4afbb573d80a72ff47
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-13 13:31:56 -0400

    forgot password working

---
+++ pages/api/passwords/forgot.js
@@ -0,0 +1,44 @@
+import User from '../../../models/userSchema'
+import nc from 'next-connect'
+import ncOptions from '../../../utils/ncUtils'
+import { dbConnectMiddleware } from '../../../utils/dbConnect'
+import Email from '../../../utils/email'
+
+const handler = nc(ncOptions)
+	.use(dbConnectMiddleware)
+	.post(async (req, res) => {
+		const user = await User.findOne({ email: req.body.email })
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'No user found with that email',
+			})
+		}
+
+		const resetToken = user.createPasswordResetToken()
+
+		console.log(resetToken)
+		await user.save({ validateBeforeSave: false })
+
+		try {
+			const resetUrl = `http://localhost:3000/api/passwords/reset/${resetToken}`
+
+			await new Email(user, resetUrl).sendPasswordReset()
+
+			res.status(200).json({
+				success: true,
+				message: 'Password reset link sent to email',
+			})
+		} catch (err) {
+			user.passwordResetToken = undefined
+			user.passwordResetExpires = undefined
+
+			await user.save({ validateBeforeSave: false })
+			return res.status(500).json({
+				success: false,
+				message: 'There was an error sending the email. Try again later',
+			})
+		}
+	})
+
+export default handler
