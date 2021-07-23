020ce1e0237a0d6bf87ab5fd75dd1002ec8116ff
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-23 13:35:27 -0400

    list of links

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
 import HeadEl from '../components/HeadEl'
-import styles from '../styles/index.module.css'
+import Link from 'next/link'
+import styles from '../styles/index.module.scss'
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<HeadEl
 				title="My App"
-				meta={{ description: 'My page description', content: 'content of page' }}
+				meta={{
+					description: 'My page description',
+					content: 'content of page',
+				}}
 			/>
 
-			<main className={styles.main}>Main Content</main>
+			<main className={styles.main}>
+				<ul className={styles.links}>
+					<Link href="/login">
+						<a>Login</a>
+					</Link>
+					<Link href="/gallery">
+						<a>Gallery</a>
+					</Link>
+				</ul>
+			</main>
 		</div>
 	)
 }
