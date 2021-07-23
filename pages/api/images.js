586797decea9aa95d21434f3f4f2155e58018b6a
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-23 13:38:36 -0400

    get all images

---
+++ pages/api/images.js
@@ -0,0 +1,24 @@
+import { dbConnectMiddleware } from '../../utils/dbConnect'
+import Image from '../../models/imageSchema'
+import nc from 'next-connect'
+import { protect, restrict } from '../../utils/authMiddleware'
+import ncOptions from '../../utils/ncUtils'
+
+const handler = nc(ncOptions)
+	.use(dbConnectMiddleware)
+	.get(async (req, res) => {
+		const images = await Image.find()
+
+		if (!images) {
+			return res.status(404).json({
+				success: false,
+				message: 'No images found in database',
+			})
+		} else
+			return res.status(200).json({
+				success: true,
+				images,
+			})
+	})
+
+export default handler
