11c6cc3481420077c48774a8d7b0c17fbec26c51
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-24 13:10:24 -0400

    fixed window errs

--- utils/client/auth.js
+++ utils/client/auth.js
@@ -1,17 +1,27 @@
 export const auth = {
   setAuthToken: function (data, next) {
-    if (window !== undefined) {
+    if (window !== 'undefined') {
       localStorage.setItem('jwt', JSON.stringify(data))
     }
     next()
   },
   checkAuthToken: function () {
-    return typeof window == undefined
+    return typeof window == 'undefined'
       ? false
       : localStorage.getItem('jwt')
       ? JSON.parse(localStorage.getItem('jwt'))
       : false
   },
+}
+
+export const isAdmin = () => {
+  if (typeof window !== 'undefined') {
+    return auth.checkAuthToken()
+      ? auth.checkAuthToken().user.role === 'admin'
+        ? true
+        : false
+      : false
+  }
 }
 
 export default auth
