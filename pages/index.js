8c7e4a2b43dea3c1298586cf0b18120cd923c56b
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-24 13:09:55 -0400

    client-side route protection working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import HeadEl from '../components/HeadEl'
 import Link from 'next/link'
 import styles from '../styles/index.module.scss'
+import auth, { isAdmin } from '../utils/client/auth'
+
+const { checkAuthToken } = auth
 
 export default function Home() {
 	return (
@@ -15,9 +18,24 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<ul className={styles.links}>
-					<Link href="/login">
-						<a>Login</a>
-					</Link>
+					{!checkAuthToken() && (
+						<Link href="/login">
+							<a>Login</a>
+						</Link>
+					)}
+
+					{checkAuthToken() && (
+						<Link href="/logout">
+							<a>Logout</a>
+						</Link>
+					)}
+
+					{isAdmin() && (
+						<Link href="/uploadImages">
+							<a>Upload Image</a>
+						</Link>
+					)}
+
 					<Link href="/gallery">
 						<a>Gallery</a>
 					</Link>
